e9c87dce9ecdc19bfa56d04359cd11ac916de677
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 16:16:14 +0530

    Added Navbar

---
+++ src/components/nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav class="navbar sticky-top navbar-light bg-dark">
+      <span class="navbar-brand mb-0 h1 text-white">Emoji Dictionary</span>
+    </nav>
+  );
+}
+
+export default Nav;

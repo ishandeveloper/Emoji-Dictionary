77b1579e1aaaab8400a0b58b86b59828bd8e987f
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 14:49:03 +0530

    Card

---
+++ src/components/card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+function Card() {
+  return (
+    <div className="col-md-3">
+      <div className="card">
+        <div className="card-img-container">😀</div>
+
+        <div className="card-body">
+          <h5 className="card-title">Grinning Face</h5>
+          <p className="card-text">
+            The happy smiling face is one of the most common emojis and
+            universally applicable: you just want to say hello, express joy or
+            excitement about something or brighten up a short text.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;

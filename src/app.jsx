5b38cfcc9c508fb91c923d332eac262ef8af8213
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 16:16:22 +0530

    Added Navbar

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Card from "./components/card";
 import emojidata from "./emojidata";
+import Nav from './components/nav';
 function App() {
-  return <div className="row">{emojidata.map(Card)}</div>;
+  return <div>
+      <Nav/>
+      <div className="row">{emojidata.map(Card)}</div>;
+  </div>
 }
 
 export default App;

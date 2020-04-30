a1f18fce3ef40394a3b6ecdd9150dec0b887c659
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 14:48:56 +0530

    Data

---
+++ src/emojidata.js
@@ -0,0 +1,10 @@
+const emojidata = [
+  {
+    id: 1,
+    emoji: "😀",
+    name: "Grinning",
+    info:
+      "The happy smiling face is one of the most common emojis and universally applicable: you just want to say hello, express joy or excitement about something or brighten up a short text.",
+  },
+];
+export default emojidata;

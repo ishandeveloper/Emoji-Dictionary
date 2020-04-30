0339281bc191918d8b70f494617d34b1e66935aa
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 16:21:06 +0530

    Added Credits

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function Nav() {
   return (
     <nav class="navbar sticky-top navbar-light bg-dark">
-      <span class="navbar-brand mb-0 h1 text-white">Emoji Dictionary</span>
+      <span class="navbar-brand mb-0 h1 text-white">
+      📖 <div className="hero-text">
+          Emoji Dictionary<br/>
+          <div className="credits">by <b><a href="http://ishandeveloper.com">ishandeveloper</a></b></div>
+          </div>
+      </span>
     </nav>
   );
 }
